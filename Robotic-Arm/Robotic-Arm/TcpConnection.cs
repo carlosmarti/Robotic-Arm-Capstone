fce6d36fec85392ab985d8a8754e570be6a9576b
carlosmarti <carlos.marti29@yahoo.com>
2020-02-13 10:34:36 -0500

    inserted while loop to get multiple messages from client

--- Robotic-Arm/Robotic-Arm/TcpConnection.cs
+++ Robotic-Arm/Robotic-Arm/TcpConnection.cs
@@ -12,36 +12,40 @@ namespace Robotic_Arm
     {
         //Memeber variables
         private int port = 81;
+        private bool running = true;
 
         public TcpConnection()
         {
+            //instance listener to commumicate with server
+            var ipAddress = System.Net.IPAddress.Any;
+            var listener = new System.Net.Sockets.TcpListener(ipAddress, port);
 
             try
             {
 
-                //instance listener to commumicate with server
-                var ipAddress = System.Net.IPAddress.Any;
-                var listener = new System.Net.Sockets.TcpListener(ipAddress, port);
+                
                 Console.WriteLine("Listening...");
 
                 //start listening for server messages
                 listener.Start();
-
                 //get client tryng to send message
                 var client = listener.AcceptTcpClient();
 
+                while (running)
+                {
+                
+                    NetworkStream netS = client.GetStream();
 
-                NetworkStream netS = client.GetStream();
+                    byte[] buffer = new byte[client.ReceiveBufferSize];
+                    int bytesRead = netS.Read(buffer, 0, client.ReceiveBufferSize);
 
-                byte[] buffer = new byte[client.ReceiveBufferSize];
-                int bytesRead = netS.Read(buffer, 0, client.ReceiveBufferSize);
+                    string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("received: {0}", dataReceived);
 
-                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                Console.WriteLine("received: {0}", dataReceived);
-
-                //write back to server
-                Console.WriteLine("Sending data back");
-                netS.Write(buffer, 0, bytesRead);
+                    //write back to server
+                    Console.WriteLine("Sending data back");
+                    netS.Write(buffer, 0, bytesRead);
+                }
 
                 //close all open connections
                 client.Close();
