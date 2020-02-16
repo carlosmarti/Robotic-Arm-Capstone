fdb14db7135e0a3d3f5487977056b3ff860d75c4
carlosmarti <carlos.marti29@yahoo.com>
2020-02-16 09:18:59 -0500

    finished refactor of tcpConnection file

--- Robotic-Arm/Robotic-Arm/TcpConnection.cs
+++ Robotic-Arm/Robotic-Arm/TcpConnection.cs
@@ -9,15 +9,16 @@ using System.Threading;
 
 namespace Robotic_Arm
 {
-    class TcpConnection
+    static class TcpConnection
     {
         //Memeber variables
-        private int port;  //81
-        private bool running = true;
+        private static int port;  //81
+        private static bool running = false;
 
-        TcpListener listener;
+        private static TcpListener listener;
+        private static Thread serverThread;
 
-        public TcpConnection(int portNum)
+        public static void Init(int portNum)
         {
             port = portNum;
             //instance listener to commumicate with server
@@ -38,11 +39,11 @@ namespace Robotic_Arm
             }
         }
         
-        public void Start()
+        public static void Start()
         {
             try
             {
-                Thread serverThread = new Thread(new ThreadStart(Run));
+                serverThread = new Thread(new ThreadStart(Run));
                 serverThread.Start();
             }
             catch(Exception e)
@@ -51,36 +52,47 @@ namespace Robotic_Arm
             }
         }
 
-        public void Run()
+        public static void Run()
         {
             //get client tryng to send message
             var client = listener.AcceptTcpClient();
+            running = true;
 
-            while (running)
+            try
             {
+                while (running)
+                {
 
-                NetworkStream netS = client.GetStream();
+                    NetworkStream netS = client.GetStream();
 
-                byte[] buffer = new byte[client.ReceiveBufferSize];
-                int bytesRead = netS.Read(buffer, 0, client.ReceiveBufferSize);
+                    byte[] buffer = new byte[client.ReceiveBufferSize];
+                    int bytesRead = netS.Read(buffer, 0, client.ReceiveBufferSize);
 
-                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                Console.WriteLine("received: {0}", dataReceived);
+                    string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("received: {0}", dataReceived);
 
-                //write back to server
-                Console.WriteLine("Sending data back");
-                netS.Write(buffer, 0, bytesRead);
+                    //write back to server
+                    Console.WriteLine("Sending data back");
+                    netS.Write(buffer, 0, bytesRead);
+                }
             }
-
-            //close all open connections
-            client.Close();
-            listener.Stop();
-            Console.ReadLine();
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                //close all open connections
+                client.Close();
+                listener.Stop();
+                Console.ReadLine();
+            }
+            
         }
 
-        public void Stop()
+        public static void Stop()
         {
-
+            running = false;
         }
 
 
