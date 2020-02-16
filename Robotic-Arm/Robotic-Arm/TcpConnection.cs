1a87ed3df33331fff973ed71cb3d15b0740a5dd4
carlosmarti <carlos.marti29@yahoo.com>
2020-02-16 06:49:35 -0500

    started refactoring TcpConnection to better work with GUI

--- Robotic-Arm/Robotic-Arm/TcpConnection.cs
+++ Robotic-Arm/Robotic-Arm/TcpConnection.cs
@@ -5,58 +5,85 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace Robotic_Arm
 {
     class TcpConnection
     {
         //Memeber variables
-        private int port = 81;
+        private int port;  //81
         private bool running = true;
 
-        public TcpConnection()
+        TcpListener listener;
+
+        public TcpConnection(int portNum)
         {
+            port = portNum;
             //instance listener to commumicate with server
             var ipAddress = System.Net.IPAddress.Any;
-            var listener = new System.Net.Sockets.TcpListener(ipAddress, port);
 
             try
             {
+                listener = new System.Net.Sockets.TcpListener(ipAddress, port);
 
-                
                 Console.WriteLine("Listening...");
-
                 //start listening for server messages
                 listener.Start();
-                //get client tryng to send message
-                var client = listener.AcceptTcpClient();
-
-                while (running)
-                {
-                
-                    NetworkStream netS = client.GetStream();
-
-                    byte[] buffer = new byte[client.ReceiveBufferSize];
-                    int bytesRead = netS.Read(buffer, 0, client.ReceiveBufferSize);
-
-                    string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                    Console.WriteLine("received: {0}", dataReceived);
-
-                    //write back to server
-                    Console.WriteLine("Sending data back");
-                    netS.Write(buffer, 0, bytesRead);
-                }
-
-                //close all open connections
-                client.Close();
-                listener.Stop();
-                Console.ReadLine();
+           
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
         }
+        
+        public void Start()
+        {
+            try
+            {
+                Thread serverThread = new Thread(new ThreadStart(Run));
+                serverThread.Start();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public void Run()
+        {
+            //get client tryng to send message
+            var client = listener.AcceptTcpClient();
+
+            while (running)
+            {
+
+                NetworkStream netS = client.GetStream();
+
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+                int bytesRead = netS.Read(buffer, 0, client.ReceiveBufferSize);
+
+                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("received: {0}", dataReceived);
+
+                //write back to server
+                Console.WriteLine("Sending data back");
+                netS.Write(buffer, 0, bytesRead);
+            }
+
+            //close all open connections
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
+        }
+
+        public void Stop()
+        {
+
+        }
+
+
     }
 }
 
