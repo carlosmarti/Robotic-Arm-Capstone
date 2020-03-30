5a10a593ffb8ec71e3765ec24713a4adc3e6e135
carlosmarti <carlos.marti29@yahoo.com>
2020-03-30 13:05:36 -0400

    added closure to the database

--- Robotic-Arm/AdminPage/php/getData.php
+++ Robotic-Arm/AdminPage/php/getData.php
@@ -3,17 +3,17 @@
     $host = "localhost";
     $userName = "root";
     $pass = "";
-    $dbName = "RobotTesting";
+    $dbName = "robottesting";
 
     $con = mysqli_connect($host, $userName, $pass, $dbName);
 
     if(!$con)
-    {
         die('Could not connect: '. mysqli_error($con));
-    }
     else
         echo "Connected!";
 
+    mysqli_select_db($con, "endurancetesting");
 
+    mysqli_close($con);
 
 ?>
